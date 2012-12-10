56b73d076ac927b0cd77c7e20fd9ab0ce5350f06
Jim Knight <jim.knight@lavatech.com>
2012-12-09 20:44:22 -0500

    got an actual barcode scan working

--- Rakefile
+++ Rakefile
@@ -6,4 +6,11 @@ require 'formotion'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'Wino'
+  app.identifier = "com.youreawino.wino"
+  app.provisioning_profile = "/Users/jimknight/Library/MobileDevice/Provisioning Profiles/brother.mobileprovision"
+  app.vendor_project('vendor/zxing-2.0/iphone/ZXingWidget', :xcode, :target => 'ZXingWidget', :headers_dir => 'Classes')
+  app.frameworks += ['AddressBook','AddressBookUI','AudioToolbox','AVFoundation','CoreMedia','CoreVideo']
+  app.libs += ['/usr/lib/libiconv.dylib']
+  
+  # app.codesign_certificate = "iPhone Developer: Lava Tech Inc (46VXBRL66A)"
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,16 +1,23 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+        @scannerViewController = ScannerViewController.alloc.init
+    
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-
-    @navigation_controller = UINavigationController.alloc.initWithRootViewController(UserController.controller)
-    @window.rootViewController = @navigation_controller
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@scannerViewController)
+    @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
-
-    @login = LoginController.alloc.init
-    @login_navigation = UINavigationController.alloc.initWithRootViewController(@login)
-
-    UserController.controller.presentModalViewController(@login_navigation, animated:false)
-
     true
+    # @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    # @navigation_controller = UINavigationController.alloc.initWithRootViewController(UserController.controller)
+    # @window.rootViewController = @navigation_controller
+    # @window.makeKeyAndVisible
+
+    # @login = LoginController.alloc.init
+    # @login_navigation = UINavigationController.alloc.initWithRootViewController(@login)
+
+    # UserController.controller.presentModalViewController(@login_navigation, animated:false)
+
+    # true
   end
 end
